dae4907a1687f03adc09727d9d9ca2af480738f4
Manuel Kraus <manuel@kraus.rocks>
2017-02-14 17:11:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# VPlanClient
+
+VPlanClient watches a folder and uploads every new/edited file to a webserver
+
+## Installing
+(Assuming you already have a working go directory)
+`go get github.com/mpggu/vplanclient`
+
+## Usage
+`./VPlanClient -url="http://foo.bar" -folder:"~/vplan" -auth="XXX_secret"`
+
+
+(Pardon the horrible code, this is my first go "project")
---
+++ VPlanClient.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func isCreateOrWrite(op fsnotify.Op) bool {
+	return op&fsnotify.Write == fsnotify.Write || op&fsnotify.Create == fsnotify.Create
+}
+
+func main() {
+	var url = flag.String("url", "", "The URL to post the VPlan data to")
+	var folder = flag.String("folder", "", "The folder to watch")
+	var auth = flag.String("auth", "", "The authentication secret")
+
+	flag.Parse()
+	log.Println(*url, *folder)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if isCreateOrWrite(event.Op) {
+					log.Println("modified file: ", event.Name)
+					b, err := ioutil.ReadFile(event.Name)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					postVPlan(*url, toUtf8(b), *auth)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(*folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
+func toUtf8(buffer []byte) string {
+	buf := make([]rune, len(buffer))
+	for i, b := range buffer {
+		buf[i] = rune(b)
+	}
+	return string(buf)
+}
+
+func postVPlan(url, data, auth string) {
+	if len(data) < 20 {
+		return
+	}
+
+	values := map[string]string{"vplan": data}
+
+	jsonValue, _ := json.Marshal(values)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", auth)
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	defer resp.Body.Close()
+}
